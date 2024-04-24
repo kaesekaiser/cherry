d014c1d17755de1f2f0b965acd7b3c39797545b0
kaesekaiser <cbain919@gmail.com>
2024-04-24 01:00:15 -0400

    Byte arrays and some generalization

--- bytes.py
+++ bytes.py
@@ -1,53 +1,76 @@
-def convert_to_bits(data: int | str | list[int], length: int = 8) -> list[int]:
+class BinaryError(ValueError):
+    pass
+
+
+SupportsBitConversion = int | str | list[int]
+
+
+def convert_to_bits(data: SupportsBitConversion, length: int = 8) -> list[int]:
     if isinstance(data, int):
         return [int(g) for g in f"{data:b}".rjust(length, "0")[:-(length+1):-1]]
     elif isinstance(data, str):
         if not all(c in "01" for c in data):
-            raise ValueError("The value of a bit must be either 0 or 1.")
+            raise BinaryError("The value of a bit must be either 0 or 1.")
         return [int(g) for g in data.rjust(length, "0")[:-(length+1):-1]]
     elif isinstance(data, list):
         return data[:length] + [0 for _ in range(len(data), length)]
+    elif isinstance(data, Byte):
+        return data.bits
 
 
 class Byte:  # not a fan of the built-in binary classes
-    def __init__(self, value: int | str | list[int] = 0):
-        if value:
-            self.bits = convert_to_bits(value)
+    def __init__(self, data: SupportsBitConversion = 0, size: int = 8):
+        self.size = size
+        if data:
+            self.bits = convert_to_bits(data, length=self.size)
         else:
-            self.bits = [0 for _ in range(8)]
+            self.bits = [0 for _ in range(self.size)]
+
+    @staticmethod
+    def from_list(ls: list[int]):
+        return Byte(data=ls, size=len(ls))
+
+    @property
+    def is_array(self):
+        return self.size * 8 == len(self.bits)
 
     @property
     def value(self):
         return int("".join(str(g) for g in self.bits.__reversed__()), 2)
 
+    def __len__(self):
+        return len(self.bits)
+
     def __neg__(self):
-        return Byte([(1, 0)[g] for g in self.bits])
+        return self.__class__(data=[[1, 0][g] for g in self.bits], size=self.size)
 
     def __and__(self, other):
-        return Byte(self.value & other.value)
+        return self.__class__(data=self.value & other.value, size=self.size)
 
     def __or__(self, other):
-        return Byte(self.value | other.value)
+        return self.__class__(data=self.value | other.value, size=self.size)
 
     def __xor__(self, other):
-        return Byte(self.value ^ other.value)
+        return self.__class__(data=self.value ^ other.value, size=self.size)
 
     def __eq__(self, other):
-        return self.value == other.value
+        return self.bits == other.bits
 
-    def __lshift__(self, other):
-        return Byte(self.value << other)
+    def __lshift__(self, n):
+        return self.__class__(data=[0 for _ in range(n)] + self.bits[:-n], size=self.size)
 
-    def __rshift__(self, other):
-        return Byte(self.value >> other)
+    def __rshift__(self, n):
+        return self.__class__(data=self.bits[n:] + [0 for _ in range(n)], size=self.size)
 
     def __bytes__(self):
         return bytes(self.value)
 
     def __str__(self):
-        return f"{self.value:b}".rjust(8, "0")
+        return f"{self.value:b}".rjust(len(self), "0")
 
     def __getitem__(self, item: int | slice):
+        if isinstance(item, slice):
+            return Byte.from_list(self.bits[item])
         return self.bits[item]
 
     def __setitem__(self, key, value):
@@ -57,8 +80,30 @@ class Byte:  # not a fan of the built-in binary classes
             self.bits[key] = data
         elif isinstance(key, int):
             if value != 0 and value != 1:
-                raise ValueError("The value of a bit must be either 0 or 1.")
+                raise BinaryError("The value of a bit must be either 0 or 1.")
             self.bits[key] = value
 
-    def flip(self, index):
+    def flip(self, index: int):
         self.bits[index] = int(not self.bits[index])
+
+
+class ByteArray(Byte):
+    def __init__(self, size: int, data: SupportsBitConversion = 0):
+        super().__init__(size=size*8, data=data)
+        self.size = size
+
+    @staticmethod
+    def from_bytes(bts: list[Byte]):
+        return ByteArray(len(bts), [j for g in bts for j in g.bits])
+
+    @property
+    def bytes(self) -> list[Byte]:
+        return [Byte(self.bits[g*8:g*8+8]) for g in range(self.size)]
+
+    def __str__(self):
+        return " ".join(str(g) for g in self.bytes)
+
+    def __getitem__(self, item) -> Byte:
+        if isinstance(item, slice):
+            return Byte.from_list(self.bits[item])
+        return self.bytes[item]

45febe5996d55e23d46f97221948b0f6c51855a4
kaesekaiser <cbain919@gmail.com>
2024-04-26 02:21:04 -0400

    Basic stack operations

--- bytes.py
+++ bytes.py
@@ -1,21 +1,41 @@
-class BinaryError(ValueError):
+import json
+import re
+from math import ceil
+
+
+opcodes = json.load(open("opcodes.json"))
+mnemonics = {j["code"]: g for g, j in opcodes.items()}
+
+
+class BitError(ValueError):
     pass
 
 
 SupportsBitConversion = int | str | list[int]
 
 
+def zero_pad(ls: list, length: int):
+    return ls[:length] + [0 for _ in range(len(ls), length)]
+
+
 def convert_to_bits(data: SupportsBitConversion, length: int = 8) -> list[int]:
     if isinstance(data, int):
         return [int(g) for g in f"{data:b}".rjust(length, "0")[:-(length+1):-1]]
     elif isinstance(data, str):
-        if not all(c in "01" for c in data):
-            raise BinaryError("The value of a bit must be either 0 or 1.")
-        return [int(g) for g in data.rjust(length, "0")[:-(length+1):-1]]
+        if re.fullmatch(r"[01]{8}( +[01]{8})*", data):
+            return zero_pad([int(j) for g in data.split() for j in g[::-1]], length)
+        elif re.fullmatch(r"0b[01]*", data):
+            return [int(g) for g in data[2:].rjust(length, "0")[:-(length+1):-1]]
+        elif re.fullmatch(r"[0-9A-Fa-f]{2}( +[0-9A-Fa-f]{2})*", data):
+            return zero_pad([j for g in data.split() for j in convert_to_bits(int(g, 16), 8)], length)
+        elif re.fullmatch(r"0x[0-9A-Fa-f]*", data):
+            return zero_pad([j for g in data[2:] for j in convert_to_bits(int(g, 16), 4)], length)
+        else:
+            raise BitError(f"Invalid string format: {data}")
     elif isinstance(data, list):
-        return data[:length] + [0 for _ in range(len(data), length)]
+        return zero_pad(data, length)
     elif isinstance(data, Byte):
-        return data.bits[:length] + [0 for _ in range(len(data), length)]
+        return zero_pad(data.bits, length)
 
 
 class Byte:  # not a fan of the built-in binary classes
@@ -34,6 +54,10 @@ class Byte:  # not a fan of the built-in binary classes
     @property
     def value(self):
         return int("".join(str(g) for g in self.bits.__reversed__()), 2)
+
+    @property
+    def mnemonic(self) -> str:
+        return mnemonics.get(self.value)
 
     def __len__(self):
         return len(self.bits)
@@ -62,6 +86,10 @@ class Byte:  # not a fan of the built-in binary classes
     def __bytes__(self):
         return bytes(self.value)
 
+    @property
+    def hex(self):
+        return hex(self.value)[2:].upper().rjust(2, "0")
+
     def __str__(self):
         return f"{self.value:b}".rjust(len(self), "0")
 
@@ -77,7 +105,7 @@ class Byte:  # not a fan of the built-in binary classes
             self.bits[key] = data
         elif isinstance(key, int):
             if value != 0 and value != 1:
-                raise BinaryError("The value of a bit must be either 0 or 1.")
+                raise BitError("The value of a bit must be either 0 or 1.")
             self.bits[key] = value
 
     def flip(self, index: int):
@@ -93,14 +121,26 @@ class ByteArray(Byte):
     def from_bytes(bts: list[Byte]):
         return ByteArray(len(bts), [j for g in bts for j in g.bits])
 
+    @staticmethod
+    def from_list(ls: list[int]):
+        return ByteArray(data=ls, size=ceil(len(ls) / 8))
+
     @property
     def bytes(self) -> list[Byte]:
         return [Byte(self.bits[g*8:g*8+8]) for g in range(self.size)]
+
+    @property
+    def hex(self):
+        return " ".join(g.hex for g in self.bytes)
+
+    @property
+    def mnemonic(self) -> str:
+        return mnemonics.get(self[0].value)
 
     def __str__(self):
         return " ".join(str(g) for g in self.bytes)
 
     def __getitem__(self, item) -> Byte:
         if isinstance(item, slice):
-            return Byte.from_list([j for g in self.bytes[item] for j in g.bits])
+            return ByteArray.from_list([j for g in self.bytes[item] for j in g.bits])
         return self.bytes[item]
--- registers.py
+++ registers.py
@@ -1,4 +1,3 @@
-import json
 from bytes import *
 
 
@@ -12,20 +11,40 @@ class Register:
     def bytes(self):
         return ByteArray(self.size, self.bits)
 
+    @property
+    def value(self):
+        return self.bytes.value
+
+    @property
+    def hex(self):
+        return self.bytes.hex
+
     @staticmethod
     def from_json(js: dict):
         return Register(size=js["size"], data=js.get("data", 0), children=js.get("children", {}))
 
+    def __getitem__(self, item) -> Byte | ByteArray:
+        return self.bytes[item]
+
     def __str__(self):
-        return str(ByteArray(self.size, self.bits))
+        return str(self.bytes)
 
     def write(self, data: SupportsBitConversion | Byte):
         self.bits = convert_to_bits(data, length=self.size * 8)
 
 
 class Drive(Register):
-    def write_at(self, address: int, data: SupportsBitConversion | Byte):
-        self.bits[address:address+8] = convert_to_bits(data)
+    def read(self, address: int | ByteArray, no_bytes: int) -> Byte:
+        if isinstance(address, ByteArray):
+            address = address.value
+        return ByteArray.from_list(self.bits[address*8:(address+no_bytes)*8])
+
+    def write_at(self, address: int | ByteArray, data: SupportsBitConversion | Byte, no_bytes: int):
+        if isinstance(address, ByteArray):
+            address = address.value
+        self.bits[address*8:(address+no_bytes)*8] = convert_to_bits(data, no_bytes * 8)
+        if address + no_bytes > self.size:
+            del self.bits[self.size*8:]
 
 
 class Machine:
@@ -33,12 +52,67 @@ class Machine:
         registers = json.load(open("registers.json"))
         self.registers = {k: Register.from_json(v) for k, v in registers.items()}
         self.register_pointers = {v["pointer"]: k for k, v in registers.items()}
-        self.memory = Register(65536)  # just implementing memory as a continuous linear address space w/o paging
+        self.memory = Drive(65536)  # just implementing memory as a continuous linear address space w/o paging
+        self.operation_counter = 0
 
-    def get_register(self, code: str | int) -> Register:
+    @property
+    def state_map(self):
+        return f"  [AG] {self.get_register('AG').hex}    [IP] {self.get_register('IP').hex}\n" \
+               f"  [BG] {self.get_register('BG').hex}    [SP] {self.get_register('SP').hex}\n" \
+               f"  [CG] {self.get_register('CG').hex}    [FL] {self.get_register('FL')}\n"
+
+    def get_register(self, code: str | int | Byte) -> Register:
+        if isinstance(code, Byte):
+            code = code.value
         return self.registers[self.register_pointers.get(code, code)]
 
-    def write_to_register(self, pointer: str | int, data: SupportsBitConversion | Byte):
+    def write_to_register(self, pointer: str | int | Byte, data: SupportsBitConversion | Byte):
+        if isinstance(pointer, Byte):
+            pointer = pointer.value
         (register := self.get_register(pointer)).write(data)
         for k, v in register.children.items():
             self.get_register(k).write(register.bytes[v:])
+
+    def read_stack(self, no_bytes: int = 4):
+        return self.memory.read(self.get_register("SP").value, no_bytes)
+
+    def execute_instruction(self, instruction: ByteArray):
+        """Executes the instruction written into the given ByteArray.
+
+        This function is an extreme abstraction of the process, obviously."""
+
+        mnemonic = instruction.mnemonic
+        core = mnemonic.split("-")[0]
+        suffix = mnemonic.split("-")[1] if "-" in mnemonic else ""
+
+        if core == "PUSH":
+            length = 4 if suffix[1] == "W" else 1
+            if suffix[0] == "R":
+                content = self.get_register(instruction[1]).bytes
+            elif suffix[0] == "A":
+                content = self.memory.read(instruction[1], length)
+            else:
+                content = instruction[1:1+length]
+            self.write_to_register("SP", self.get_register("SP").value - length)
+            self.memory.write_at(self.get_register("SP").value, content, length)
+
+        elif core == "POP":
+            length = 4 if suffix[0] == "W" else 1
+            self.write_to_register(instruction[1], self.read_stack(length))
+            self.write_to_register("SP", self.get_register("SP").value + length)
+
+    def run(self, address: int):
+        """Runs a program starting at the given memory address."""
+        self.write_to_register("IP", address)
+        # print(f"Initial state:\n{self.state_map}")
+        while True:
+            instruction_pointer = self.get_register("IP").value
+            next_instruction = self.memory.read(instruction_pointer, 16)
+            # print(f"Instruction: {next_instruction.hex}\n")
+            if next_instruction.mnemonic == "HLT":
+                break
+            else:
+                self.execute_instruction(next_instruction)
+            self.write_to_register("IP", instruction_pointer + opcodes[next_instruction.mnemonic]["arg_bytes"] + 1)
+            # print(f"After instruction {self.operation_counter}:\n{self.state_map}")
+            self.operation_counter += 1
